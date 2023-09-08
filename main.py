0627ff7c603fe26ce35cc640e003c96125e3c34a
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-08 09:47:37 -0300

    add arg parser

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import json
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Rinha interpreter made with Python.')
+    parser.add_argument('json_path',
+                        metavar='json_path',
+                        type=str,
+                        help='path to json AST')
+
+    args = parser.parse_args()
+
+    print(f'Hi, {args.json_path}')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
